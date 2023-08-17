236a6a39bf5eec71a375dc1fa0bd0808300785ce
Ajay Krishnamurthy <ajayvkrishnamurthy@gmail.com>
2023-08-16 23:43:53 -0400

    remove unneccesary things

--- backend/cfehome/urls.py
+++ backend/cfehome/urls.py
@@ -20,6 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/', include('api.urls')),
-    path('api/v2/', include('cfehome.routers')),
-    # path('api/products/', include('products.urls'))
 ]
--- py_client/detail.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-endpoint = "http://localhost:8000/api/products/1/"
-
-get_response = requests.get(endpoint)
-#print(get_response.text)
-print(get_response.json()) # print raw text response
-
-
